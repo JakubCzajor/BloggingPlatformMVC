b6a498f362af9c4c31c32b2bdb303e5384831b89
JakubCzajor <jakub.czajor7@gmail.com>
2024-04-25 22:02:13 +0200

    Add Category Repository

---
+++ BloggingPlatform/Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using BloggingPlatform.Data;
+using BloggingPlatform.Models;
+using BloggingPlatform.Repository.IRepository;
+
+namespace BloggingPlatform.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ BloggingPlatform/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BloggingPlatform.Models;
+
+namespace BloggingPlatform.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}

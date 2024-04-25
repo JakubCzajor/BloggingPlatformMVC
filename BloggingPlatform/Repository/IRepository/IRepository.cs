2835d3507714f3bb5a6904582236d0ce3fa42893
JakubCzajor <jakub.czajor7@gmail.com>
2024-04-25 21:57:45 +0200

    Add Repository

---
+++ BloggingPlatform/Repository/IRepository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BloggingPlatform.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ BloggingPlatform/Repository/Repository.cs
@@ -0,0 +1,40 @@
+﻿using BloggingPlatform.Data;
+using BloggingPlatform.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggingPlatform.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+    }
+}

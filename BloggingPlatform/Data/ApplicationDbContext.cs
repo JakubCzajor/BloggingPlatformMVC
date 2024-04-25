2ee7b9db60340648a406a40119b20b01e4bd980c
JakubCzajor <jakub.czajor7@gmail.com>
2024-04-25 20:06:55 +0200

    Add ApplicationDbContext

---
+++ BloggingPlatform/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BloggingPlatform.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}

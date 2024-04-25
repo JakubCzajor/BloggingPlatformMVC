89eb3305153e22007598f569b3467143b3693086
JakubCzajor <jakub.czajor7@gmail.com>
2024-04-25 22:24:31 +0200

    Add Category Controller

---
+++ BloggingPlatform/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BloggingPlatform.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatform.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+            var categoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(categoryList);
+        }
+    }
+}

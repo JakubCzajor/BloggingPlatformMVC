78d2cd29a8e67516065d525c2f4f97c07b06aae8
JakubCzajor <jakub.czajor7@gmail.com>
2024-05-05 23:42:06 +0200

    Add Post Controller

---
+++ BloggingPlatform/Controllers/PostController.cs
@@ -0,0 +1,45 @@
+﻿using BloggingPlatform.Models;
+using BloggingPlatform.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatform.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PostController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var postList = _unitOfWork.Post.GetAll().ToList();
+            return View(postList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Post obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Post.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Post created successfully.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+    }
+}

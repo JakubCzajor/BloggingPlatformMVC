e5d8e1d723c1139878a53e79008a23a98a1de5c6
JakubCzajor <jakub.czajor7@gmail.com>
2024-05-05 23:32:25 +0200

    Add Post Repository

---
+++ BloggingPlatform/Repository/IRepository/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using BloggingPlatform.Models;
+
+namespace BloggingPlatform.Repository.IRepository
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+
+    }
+}
---
+++ BloggingPlatform/Repository/PostRepository.cs
@@ -0,0 +1,15 @@
+﻿using BloggingPlatform.Data;
+using BloggingPlatform.Models;
+using BloggingPlatform.Repository.IRepository;
+
+namespace BloggingPlatform.Repository
+{
+    public class PostRepository : Repository<Post>, IPostRepository
+    {
+        private ApplicationDbContext _db;
+        public PostRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}

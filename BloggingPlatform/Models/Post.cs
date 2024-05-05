f0e126d59bf6a886a1a507c73a053f9d334065f0
JakubCzajor <jakub.czajor7@gmail.com>
2024-05-05 23:17:35 +0200

    Add Post Model

---
+++ BloggingPlatform/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BloggingPlatform.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public string Body { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+    }
+}
